7e71bee218a830178a4ec1d3ca37f8dbf97bc944
SamReinsteinWright <thelittlepixel123@gmail.com>
2024-03-25 22:42:23 -0400

    made it so you follow the mouse and fixed a bunch of issues with the sheep

--- MyStuff/game-engine/js/Play.js
+++ MyStuff/game-engine/js/Play.js
@@ -39,22 +39,44 @@ class Play extends Phaser.Scene {
             quantity: 20,
             collideWorldBounds: true,
             bounceX: 0.5,
-            bounceY: 0.5
+            bounceY: 0.5,
+            pushable: true
         });
         this.herd.children.each(function (sheep) {
             let x = Math.random() * this.sys.canvas.width;
             let y = Math.random() * this.sys.canvas.height;
             sheep.setPosition(x, y);
+            this.physics.add.overlap(this.herd, sheep, (sheep) => {
+                if (sheep.name !== this.herd.children.entries.name) {
+                    sheep.body.velocity.x += Math.random()
+                    sheep.body.velocity.y -= Math.random()
+                    console.log('hi')
+                }
+            });
+            for (let i = 0; i < this.herd.getLength(); i++) {
+
+                this.herd.getChildren()[i].name = "sheep-" + i;
+
+                if (sheep.name === 'sheep-' + i)// && (sheep.x - this.herd.getChildren()[i].x) > 30 && (sheep.y - this.herd.getChildren()[i].y) > 30 && (sheep.x - this.herd.getChildren()[i].x) < 200 && (sheep.y - this.herd.getChildren()[i].y) < 200) 
+                {
+                    // this.physics.moveToObject(sheep, (sheep.name === 'sheep-2'), 1)
+                    console.log(sheep.name)
+                }
+
+            }
         }, this);
 
         this.createAnimations();
 
+
+
         this.physics.add.collider(this.avatar, this.wall);
-        this.physics.add.collider(this.herd);
-        this.physics.add.collider(this.herd, this.ball);
-        this.physics.add.collider(this.herd, this.wall);
+        this.physics.add.collider(this.herd, this.ball, this.wall);
+        this.physics.add.collider(this.herd, this.herd);
 
         this.physics.add.overlap(this.ball, this.hole, this.win, null, this)
+
+
 
         let style = {
             fontFamily: `sans-serif`,
@@ -65,16 +87,17 @@ class Play extends Phaser.Scene {
         this.add.text(200, 50, loadingString, style)
         this.avatar.play(`avatar-idle`)
 
+
     }
+
+
     win() {
         this.scene.start(`win`);
     }
     update() {
+
         this.herd.children.each(function (sheep) {
-            let closestSheep = this.physics.closest(sheep)
-            let farthestSheep = this.physics.furthest(sheep)
             let avatarSheep = Phaser.Math.Distance.Between(sheep.x, sheep.y, this.avatar.x, this.avatar.y)
-            let sheepDistance = Phaser.Math.Distance.Between(sheep.x, sheep.y, closestSheep.position.x, closestSheep.position.y)
             if (sheep.body.velocity.x < 0) {
                 sheep.flipX = true;
             }
@@ -82,40 +105,81 @@ class Play extends Phaser.Scene {
                 sheep.flipX = false;
             }
 
-            if (sheepDistance < 200 && sheepDistance > 100) {
-                this.physics.moveToObject(sheep, closestSheep, 30)
+
+            if (sheep.x > 780) {
+                sheep.body.velocity.x -= 5
             }
-            else if (sheepDistance < 100 && sheepDistance > 50) {
-                this.physics.moveToObject(sheep, closestSheep, 20)
+            if (sheep.x < 10) {
+                sheep.body.velocity.x += 5
             }
-            else if (sheepDistance < 10 && sheepDistance > 0) {
-                this.physics.moveToObject(sheep, closestSheep, -10)
+            if (sheep.y > 580) {
+                sheep.body.velocity.y -= 5
             }
-            else if (sheepDistance > 200) {
-                this.physics.moveToObject(sheep, farthestSheep, 40)
+            if (sheep.y < 10) {
+                sheep.body.velocity.y += 5
+            }
+
+            for (let i = 0; i < this.herd.getLength(); i++) {
+                let flock = this.herd.getChildren().find(v => v.name === "sheep-" + i)
+                let flockDistance = Phaser.Math.Distance.Between(sheep.x, sheep.y, flock.x, flock.y)
+
+
+                if (flockDistance > 70) {
+                    if (flockDistance < 80 && flockDistance > 70) {
+                        this.physics.moveToObject(sheep, flock, 20)
+                        sheep.body.velocity.x += flock.body.velocity.x / 2
+                        sheep.body.velocity.y += flock.body.velocity.y / 2
+                    }
+                }
+
             }
             if (avatarSheep < 100) {
                 this.physics.moveToObject(sheep, this.avatar, -65);
             }
+            if (sheep.body.velocity.x > 0) {
+                sheep.body.velocity.x -= 0.01;
+            }
+            if (sheep.body.velocity.x < 0) {
+                sheep.body.velocity.x += 0.01;
+            }
+            if (sheep.body.velocity.y > 0) {
+                sheep.body.velocity.y -= 0.01;
+            }
+            if (sheep.body.velocity.y < 0) {
+                sheep.body.velocity.y += 0.01;
+            }
+
+
+
 
 
         }, this);
 
         this.avatar.setVelocity(0);
 
-        if (this.cursors.left.isDown) {
+        /*if (this.cursors.left.isDown) {
             this.avatar.setVelocityX(-200);
         }
         else if (this.cursors.right.isDown) {
             this.avatar.setVelocityX(200);
         }
-
+         
         if (this.cursors.up.isDown) {
             this.avatar.setVelocityY(-200);
         }
         else if (this.cursors.down.isDown) {
             this.avatar.setVelocityY(200);
+        }*/
+
+        const pointer = this.input.activePointer;
+
+
+        let pointerDistance = Phaser.Math.Distance.Between(this.avatar.x, this.avatar.y, pointer.x, pointer.y)
+        // Move at 240 px/s
+        if (pointerDistance > 10) {
+            this.physics.moveToObject(this.avatar, pointer, 150);
         }
+
         if (this.avatar.body.velocity.x !== 0 || this.avatar.body.velocity.y !== 0) {
             if (this.avatar.body.velocity.x < 0) {
                 this.avatar.flipX = true;
